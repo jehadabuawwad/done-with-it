9f25512f78b928a7d3b794ae4edafa330048f754
Jehad Abu Awwad <jehadabuawwad@outlook.com>
2022-01-25 17:15:37 +0200

    feat : dimensions of application retrieved

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  Platform,
+} from "react-native";
 
 export default function App() {
+  // Information about dimenstions of screen
+  // console.log(Dimensions.get("screen"));
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style='auto' />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "dodgerblue",
+    width: "50%",
+    height: 70,
+    marginTop: Platform.OS === "android" ? "8%" : 0,
   },
 });
