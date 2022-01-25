f9376e51df1858d24929a46da742828689107203
Jehad Abu Awwad <jehadabuawwad@outlook.com>
2022-01-25 17:48:16 +0200

    feat : detecting orientation changing

--- App.tsx
+++ App.tsx
@@ -1,32 +1,24 @@
 import { StatusBar } from "expo-status-bar";
-import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-  Dimensions,
-  Platform,
-} from "react-native";
-
+import { SafeAreaView, Text, View, Dimensions, Platform } from "react-native";
+import { useDeviceOrientation } from "@react-native-community/hooks";
 export default function App() {
+  const { landscape } = useDeviceOrientation();
   // Information about dimenstions of screen
   // console.log(Dimensions.get("screen"));
 
   return (
     <SafeAreaView>
-      <View style={styles.container}>
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: "100%",
+          height: landscape ? "100%" : "30%",
+          marginTop: Platform.OS === "android" ? 25 : 0,
+        }}
+      >
         <Text>Open up App.tsx to start working on your app!</Text>
         <StatusBar style='auto' />
       </View>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "dodgerblue",
-    width: "100%",
-    height: 70,
-    marginTop: Platform.OS === "android" ? "8%" : 0,
-  },
-});
