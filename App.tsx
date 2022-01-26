2ffd43f24f22e4da7ac488dc1dda770fe6bfa516
Jehad Abu Awwad <jehadabuawwad@outlook.com>
2022-01-26 17:05:30 +0200

    prep : style of margin and padding

--- App.tsx
+++ App.tsx
@@ -16,6 +16,25 @@ export default function App() {
           shadowOffset: { width: -10, height: 10 },
           shadowOpacity: 0.5,
           shadowRadius: 10,
+          padding: 15,
+        }}
+      >
+        <View
+          style={{
+            width: 50,
+            height: 50,
+            backgroundColor: "gold",
+            borderRadius: 25,
+          }}
+        ></View>
+      </View>
+      <View
+        style={{
+          width: 100,
+          height: 100,
+          borderRadius: 50,
+          backgroundColor: "tomato",
+          margin: 20,
         }}
       ></View>
     </View>
