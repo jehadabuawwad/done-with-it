0e264bbb91f3e23e9885974f32f0511f50e06498
Jehad Abu Awwad <jehadabuawwad@outlook.com>
2022-02-14 18:41:35 +0200

    refactor : clean up code

--- app/components/Screen.tsx
+++ app/components/Screen.tsx
@@ -1,6 +1,8 @@
 import Constants from "expo-constants";
 import { StyleSheet, SafeAreaView } from "react-native";
 
+import colors from "../config/colors";
+
 interface IScreenProps {}
 
 const Screen: React.FunctionComponent<IScreenProps> = ({ children }) => {
@@ -8,6 +10,9 @@ const Screen: React.FunctionComponent<IScreenProps> = ({ children }) => {
 };
 
 const styles = StyleSheet.create({
-  screen: { paddingTop: Constants.statusBarHeight },
+  screen: {
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: colors.cardBackground,
+  },
 });
 export default Screen;
