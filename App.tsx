462b10f5b61057e21be7b51270acffc3010d9615
Jehad Abu Awwad <jehadabuawwad@outlook.com>
2022-02-14 11:52:44 +0200

    feat : textInput and Screen Component added

--- App.tsx
+++ App.tsx
@@ -1,25 +1,31 @@
-import { StyleSheet, View } from "react-native";
+import { StatusBar, Platform, StyleSheet } from "react-native";
 
 import Card from "./app/components/Card";
 
 import colors from "./app/config/colors";
 import DetailsScreen from "./app/screens/ListingDetailsScreen";
 import MessagesScreen from "./app/screens/MessageScreen";
+import { AppTextInput } from "./app/components/AppTextInput";
+import Constants from "expo-constants";
 
+import Screen from "./app/components/Screen";
 const DoneWithIt = () => {
+  console.log();
+
   return (
     // <View style={styles.container}>
-    //   {/* <Card
+    //   <Card
     //     title='Red jacket for sale'
     //     subTitle='100$'
     //     image={require("./app/assets/card_photos/jacket.jpg")}
-    //   /> */}
-    //   {/* <DetailsScreen /> */}
-
+    //   />
+    //   <DetailsScreen />
+    //   <MessagesScreen />
     // </View>
-    <>
-      <MessagesScreen />
-    </>
+
+    <Screen>
+      <AppTextInput placeholder="UserName" icon='email' />
+    </Screen>
   );
 };
 
---
+++ app/components/AppTextInput.tsx
@@ -0,0 +1,45 @@
+import { Platform, StyleSheet, TextInput, View } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import colors from "../config/colors";
+
+interface IAppTextInputProps {
+  icon: any;
+  placeholder: string;
+}
+
+export const AppTextInput: React.FunctionComponent<IAppTextInputProps> = ({
+  icon,
+  ...otherProps
+}) => {
+  return (
+    <View style={styles.container}>
+      <MaterialCommunityIcons
+        name={icon}
+        size={20}
+        color={colors.medium}
+        style={styles.icon}
+      />
+      <TextInput style={styles.textInput} {...otherProps} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    backgroundColor: colors.light,
+    borderRadius: 25,
+    padding: 15,
+    marginVertical: 10,
+    width: "100%",
+  },
+  textInput: {
+    fontSize: 16,
+    color: colors.dark,
+  },
+  icon: {
+    marginRight: 10,
+    paddingTop: Platform.OS === "android" ? 5 : 0,
+  },
+});
